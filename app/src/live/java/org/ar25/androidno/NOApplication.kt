b57c7757d67e06de666ca80e70df4c763c6525bb
DarkMentat <indraua@gmail.com>
2017-01-22 10:53:01 +0200

    v1.1.1

---
+++ app/src/live/java/org/ar25/androidno/NOApplication.kt
@@ -0,0 +1,41 @@
+package org.ar25.androidno
+
+import android.app.Application
+import dagger.Component
+import org.ar25.androidno.api.DaggerRetrofitModule
+import org.ar25.androidno.db.DaggerDbModule
+import org.ar25.androidno.db.LocalStorageImpl
+import org.ar25.androidno.presenters.DetailPresenter
+import org.ar25.androidno.presenters.MainPresenter
+import org.ar25.androidno.ui.DetailActivity
+import org.ar25.androidno.ui.MainActivity
+import javax.inject.Singleton
+
+
+class NOApplication : Application() {
+
+    companion object {
+        lateinit var noAppComponent: NOAppComponent
+    }
+
+    @Singleton
+    @Component(modules = arrayOf(DaggerAppModule::class, DaggerDbModule::class, DaggerRetrofitModule::class))
+    interface NOAppComponent {
+        fun inject(presenter: MainPresenter)
+        fun inject(presenter: DetailPresenter)
+        fun inject(activity: MainActivity)
+        fun inject(activity: DetailActivity)
+        fun inject(localStorage: LocalStorageImpl)
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+
+        noAppComponent = DaggerNOApplication_NOAppComponent
+                            .builder()
+                            .daggerAppModule(DaggerAppModule(this))
+                            .daggerDbModule(DaggerDbModule(this))
+                            .daggerRetrofitModule(DaggerRetrofitModule())
+                            .build()
+    }
+}
---
+++ app/src/live/java/org/ar25/androidno/api/DaggerRetrofitModule.kt
@@ -0,0 +1,40 @@
+package org.ar25.androidno.api
+
+import dagger.Module
+import dagger.Provides
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import okhttp3.logging.HttpLoggingInterceptor.Level.BASIC
+import org.ar25.androidno.BuildConfig
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory
+import java.util.concurrent.TimeUnit.MILLISECONDS
+import javax.inject.Singleton
+
+
+@Module
+class DaggerRetrofitModule {
+
+    @Provides @Singleton
+    fun provideRetrofit(): Retrofit {
+
+        val okHttpBuilder = OkHttpClient.Builder()
+
+        okHttpBuilder
+                .connectTimeout(5000L, MILLISECONDS)
+                .readTimeout(10000L, MILLISECONDS)
+                .retryOnConnectionFailure(true)
+
+        return Retrofit.Builder()
+                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+                .addConverterFactory(HtmlPostConverterFactory())
+                .client(okHttpBuilder.build())
+                .baseUrl("http://www.ar25.org/")
+                .build()
+    }
+
+    @Provides @Singleton
+    fun providePostsApi(retrofit: Retrofit): NOPostsApi {
+        return retrofit.create(NOPostsApi::class.java)
+    }
+}
